2830b88d07ee08341c768239f6b3b73180f7825f
Pavel Zanchuk and Tristan Setha <tristan@>
2019-01-15 13:25:01 -0800

    added buttons to change background color

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,21 @@ $(document).ready(function() {
     $("#b2-showing").slideToggle();
     $("#b2-hidden").slideToggle();
   });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#gray").click(function() {
+    $("body").removeClass();
+  });
 });
